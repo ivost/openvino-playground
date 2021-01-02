4e25aa07253f27e5fda13dd7e5dbcc8d9bf92212
Ivo Stoyanov <ivostoy@gmail.com>
2021-01-01 18:03:43 -0800

    wip

---
+++ python/classify/args.py
@@ -0,0 +1,43 @@
+import os
+
+from envargparse import EnvArgParser, EnvArgDefaultsHelpFormatter
+
+
+def parse_args(name):
+    parser = EnvArgParser(prog=name, formatter_class=EnvArgDefaultsHelpFormatter)
+    parser.add_argument("-i", "--input", help="Required. Path to a image or folder with images.",
+                        env_var="INPUT",
+                        default="./images",
+                        type=str)
+    parser.add_argument("-m", "--model", help="Required. Path to an .xml or .onnx file with a trained model.",
+                        env_var="MODEL",
+                        default="./models/squeezenet1.1/FP16/squeezenet1.1.xml",
+                        type=str)
+    parser.add_argument("--labels", help="Optional. Path to a labels mapping file",
+                        env_var="LABELS",
+                        default="./models/squeezenet1.1/FP16/squeezenet1.1.labels",
+                        type=str)
+    parser.add_argument("-d", "--device",
+                        env_var="DEVICE",
+                        help="Optional. Target device: CPU, GPU, FPGA, HDDL, MYRIAD or HETERO.",
+                        default="CPU", type=str)
+    parser.add_argument("-s", "--start",
+                        help="Optional. Start index (when directory)", default=0, type=int)
+    parser.add_argument("-n", "--number",
+                        help="Optional. Max number of images to process", default=10, type=int)
+    parser.add_argument("-q", "--quiet",
+                        help="Optional. Quiet mode - don't write to the output", default=False,
+                        type=bool)
+    parser.add_argument("-tn", "--top", help="Optional. Number of top results", default=3, type=int)
+    #parser.add_argument('-h', '--help', action='help', default=SUPPRESS, help='Show this help message and exit.')
+    args = parser.parse_args()
+    if args.number > 100:
+        args.number = 100
+    return args
+
+# todo
+# @timeit
+# def do_process(**kwargs):
+#         name = kw.get('log_name', method.__name__.upper())
+#    return exec_net.infer(inputs={input_blob: images})
+# print("elapsed time", time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
--- python/classify/main.py
+++ python/classify/main.py
@@ -6,68 +6,61 @@ import time
 import numpy as np
 from openvino.inference_engine import IECore
 
-from inference_engine.samples.python.classify.args import build_argparser
-from inference_engine.samples.python.common import util
+from python.classify.args import parse_args
+from python.common import util
 
 
 def main():
-    args = parse_args()
+    args = init()
+    # check how many images are available
     args.count = util.count_images(args)
-
-    ie = init_engine(args)
+    # initialize openvino engine
+    engine = init_engine(args)
 
     # Read and pre-process input images
     images = util.load_images(args)
 
     # Loading model to the plugin
-    network = ie.load_network(args.net, args.device)
+    network = engine.load_network(args.net, args.device)
 
-    # Start sync inference
     log.info("Starting inference in synchronous mode")
     start_time = time.time()
+    # inference
     res = network.infer(inputs={args.input_blob: images})
     elapsed_time = time.time() - start_time
-    log.info("elapsed time: {:.3} sec".format(elapsed_time))
-    log.info("     average: {:.3} ms".format(1000 * elapsed_time / args.count))
     if not args.quiet:
-        show_results(args, res, args.count, args.out_blob)
+        show_results(args, res)
+        log.info("elapsed time: {:.3} sec".format(elapsed_time))
+        log.info("     average: {:.3} ms".format(1000 * elapsed_time / args.count))
 
 
-def init_engine(args):
-    ie = IECore()
-    if args.cpu_extension and 'CPU' in args.device:
-        ie.add_extension(args.cpu_extension, "CPU")
-    # Read a model in OpenVINO Intermediate Representation (.xml and .bin files) or ONNX (.onnx file) format
-    log.info(f"Loading network: {args.model}")
-    net = ie.read_network(args.model)
-    assert len(net.input_info.keys()) == 1, "Sample supports only single input topologies"
-    assert len(net.outputs) == 1, "Sample supports only single output topologies"
-    args.input_blob = next(iter(net.input_info))
-    args.out_blob = next(iter(net.outputs))
-    net.batch_size = args.count
-    n, c, h, w = net.input_info[args.input_blob].input_data.shape
-    log.info("Batch size: {}".format(n))
-    args.c = c
-    args.h = h
-    args.w = w
-    args.net = net
-    return ie
-
-
-def parse_args():
+def init():
     log.basicConfig(format="[ %(levelname)s ] %(message)s", level=log.INFO, stream=sys.stdout)
-    args = build_argparser()
+    args = parse_args("classification")
     # Plugin initialization for specified device and load extensions library if specified
     log.info("Creating Inference Engine")
     return args
 
 
-def show_results(args, res, n, out_blob):
-    min_prob = 0.2
-    # Processing output blob
-    # log.info("Processing output blob")
-    res = res[out_blob]
-    # log.info("Top {} results: ".format(args.top))
+def init_engine(args):
+    engine = IECore()
+    # Read a model in OpenVINO Intermediate Representation (.xml and .bin files) or ONNX (.onnx file) format
+    log.debug(f"Loading network: {args.model}")
+    net = engine.read_network(args.model)
+    assert len(net.input_info.keys()) == 1, "Sample supports only single input topologies"
+    assert len(net.outputs) == 1, "Sample supports only single output topologies"
+    args.input_blob = next(iter(net.input_info))
+    args.out_blob = next(iter(net.outputs))
+    net.batch_size = args.count
+    n, args.c, args.h, args.w = net.input_info[args.input_blob].input_data.shape
+    log.debug("Batch size: {}".format(n))
+    args.net = net
+    return engine
+
+
+def show_results(args, result):
+    min_prob = 0.1
+    result = result[args.out_blob]
 
     if args.labels:
         with open(args.labels, 'r') as f:
@@ -75,10 +68,10 @@ def show_results(args, res, n, out_blob):
     else:
         labels_map = None
 
-    for i, probs in enumerate(res):
+    for i, probs in enumerate(result):
         probs = np.squeeze(probs)
         top_ind = np.argsort(probs)[-args.top:][::-1]
-        print("Image {}/{} - {}".format(i + 1, n, args.files[i]))
+        print("Image {}/{} - {}".format(i + 1, args.count, args.files[i]))
         for id in top_ind:
             if probs[id] < min_prob:
                 break
